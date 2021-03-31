642f863d95c7756ec3ffa514a39b07ed3af1d4a3
Sam-limyr <nacuniel@gmail.com>
2021-04-01 01:14:34 +0800

    feat (gensim): Connect model to specific input pipeline.

--- classifier_gensim.py
+++ classifier_gensim.py
@@ -1,73 +1,76 @@
-# Code taken from :https://radimrehurek.com/gensim/auto_examples/tutorials/run_doc2vec_lee.html
-
+# Code based off of :https://radimrehurek.com/gensim/auto_examples/tutorials/run_doc2vec_lee.html
 
 import gensim
 import smart_open
-import collections
-import random
+import csv
 import os
-import logging
+
+# Names of authors
+CHARLES_DICKENS_NAME = "charles_dickens"
+FYODOR_DOSTOEVSKY_NAME = "fyodor_dostoevsky"
+LEO_TOLSTOY_NAME = "leo_tolstoy"
+MARK_TWAIN_NAME = "mark_twain"
+
+author_names = [CHARLES_DICKENS_NAME, FYODOR_DOSTOEVSKY_NAME, LEO_TOLSTOY_NAME, MARK_TWAIN_NAME]
+training_files = ['paragraphs/' + author + '.csv' for author in author_names]
+
+# Set file address for test data
+TEST_DIRECTORY = 'test'
+test_files = os.listdir(os.path.join(os.getcwd(), TEST_DIRECTORY))
+test_files = [os.path.join(TEST_DIRECTORY, test_file) for test_file in test_files]
+EXPECTED_DOC_IDS = ["0: Charles Dickens",
+                    "1: Fyodor Dostoevsky",
+                    "2: Leo Tolstoy",
+                    "3: Mark Twain",
+                    "Non-matching"]
+
+# Training hyperparameters
+EPOCHS = 50
+VECTOR_SIZE = 200
+MIN_WORD_FREQ = 1  # minimum frequency of words for them to be considered; set to 1 for default
+
+# Diagnostic parameters
+TEXT_HEAD_LEN = 10
 
 
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-logger = logging.getLogger()
-
-
-# Set file names for train and test data
-test_data_dir = os.path.join(gensim.__path__[0], 'test', 'test_data')
-lee_train_file = os.path.join(test_data_dir, 'lee_background.cor')
-lee_test_file = os.path.join(test_data_dir, 'lee.cor')
-
-
-def read_corpus(fname, tokens_only=False):
+def read_corpus(fname, index_tag=None):
     with smart_open.open(fname, encoding="iso-8859-1") as f:
-        for i, line in enumerate(f):
-            tokens = gensim.utils.simple_preprocess(line)
-            if tokens_only:
-                yield tokens
-            else:
-                # For training data, add tags
-                yield gensim.models.doc2vec.TaggedDocument(tokens, [i])
+        reader = csv.reader(f)
+        tokens = []
+        for row in reader:
+            for line in row:
+                tokens += gensim.utils.simple_preprocess(line)
+        if index_tag is None:
+            return tokens
+        else:
+            return gensim.models.doc2vec.TaggedDocument(tokens, [index_tag])
 
 
-train_corpus = list(read_corpus(lee_train_file))
-test_corpus = list(read_corpus(lee_test_file, tokens_only=True))
+print("Reading data from corpus...")
+train_corpus = [read_corpus(training_file, index) for index, training_file in enumerate(training_files)]
+test_corpus = [read_corpus(test_file) for test_file in test_files]
 
-model = gensim.models.doc2vec.Doc2Vec(vector_size=50, min_count=2, epochs=10)
-
+print("Training model...")
+model = gensim.models.doc2vec.Doc2Vec(vector_size=VECTOR_SIZE, epochs=EPOCHS, min_count=MIN_WORD_FREQ)
 model.build_vocab(train_corpus)
-
 model.train(train_corpus, total_examples=model.corpus_count, epochs=model.epochs)
 
-vector = model.infer_vector(['only', 'you', 'can', 'prevent', 'forest', 'fires'])
+print("Running sanity checks on training data...")
+for train_doc_id, _ in enumerate(train_corpus):
+    inferred_train_vector = model.infer_vector(train_corpus[train_doc_id].words)
+    sims = model.dv.most_similar([inferred_train_vector], topn=len(model.dv))
 
-ranks = []
-second_ranks = []
-for doc_id in range(len(train_corpus)):
-    inferred_vector = model.infer_vector(train_corpus[doc_id].words)
-    sims = model.dv.most_similar([inferred_vector], topn=len(model.dv))
-    rank = [docid for docid, sim in sims].index(doc_id)
-    ranks.append(rank)
+    # Ensure that the closest match for a training vector is with itself
+    assert train_doc_id == sims[0][0], "Sanity check failed for document with ID {}.".format(train_doc_id)
 
-    second_ranks.append(sims[1])
+print("Testing model...")
+for test_doc_id, _ in enumerate(test_corpus):
+    inferred_test_vector = model.infer_vector(test_corpus[test_doc_id])
+    sims = model.dv.most_similar([inferred_test_vector], topn=len(model.dv))
 
-    counter = collections.Counter(ranks)
-    logger.debug(counter)
-
-    logger.debug('Document ({}): «{}»\n'.format(doc_id, ' '.join(train_corpus[doc_id].words)))
-    logger.debug(u'SIMILAR/DISSIMILAR DOCS PER MODEL %s:\n' % model)
-    for label, index in [('MOST', 0), ('SECOND-MOST', 1), ('MEDIAN', len(sims)//2), ('LEAST', len(sims) - 1)]:
-        logger.debug(u'%s %s: «%s»\n' % (label, sims[index], ' '.join(train_corpus[sims[index][0]].words)))
-
-    # Pick a random document from the test corpus and infer a vector from the model
-    doc_id = random.randint(0, len(test_corpus) - 1)
-    inferred_vector = model.infer_vector(test_corpus[doc_id])
-    sims = model.dv.most_similar([inferred_vector], topn=len(model.dv))
-
-    # Compare and print the most/median/least similar documents from the train corpus
-    logger.info('Test Document ({}): «{}»\n'.format(doc_id, ' '.join(test_corpus[doc_id])))
-    logger.info(u'SIMILAR/DISSIMILAR DOCS PER MODEL %s:\n' % model)
-    for label, index in [('MOST', 0), ('MEDIAN', len(sims)//2), ('LEAST', len(sims) - 1)]:
-        # logger.info(u'%s %s: «%s»\n' % (label, sims[index], ' '.join(train_corpus[sims[index][0]].words)))
-        logger.info(u'%s %s: «%s»\n' % (label, sims[index], ' '))
-
+    # Compare and print the scores of all documents against the training corpus
+    print('\nTesting Document ({}): «{} ...»'.format(test_doc_id, ' '.join(test_corpus[test_doc_id][:TEXT_HEAD_LEN])))
+    print('EXPECTED: {}'.format(EXPECTED_DOC_IDS[test_doc_id]))
+    for train_doc_id, score in sims:
+        print('DOC_ID {}, SCORE {}: «{} ...»'.format(train_doc_id, score,
+                                                     ' '.join(train_corpus[train_doc_id].words[:TEXT_HEAD_LEN])))
