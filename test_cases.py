9ad24d90f326adb61d8bfce396948264c7e57aeb
Sam-limyr <nacuniel@gmail.com>
2021-04-04 20:10:30 +0800

    test (test_cases): Add single-sentence, same-book test cases.

--- test_cases.py
+++ test_cases.py
@@ -2,7 +2,28 @@
 
 # Tests, from the respective authors
 
-CHARLES_DICKENS_TESTS = [
+CHARLES_DICKENS_SAME_BOOK_SINGLE_SENTENCE_TESTS = [
+    """The room remained deserted for a quarter of an hour or more; the girl
+    glided back with the same unearthly tread; and, immediately afterwards,
+    the two men were heard descending.""",
+
+    """I suppose there was a time once when my father had not
+    given matters up; but if ever there was, the time is gone.""",
+
+    """I knew my aunt sufficiently well to know that she had something of
+    importance on her mind, and that there was far more matter in this
+    arrival than a stranger might have supposed.""",
+
+    """Will you undertake to explain, that we suppose it to have been
+    addressed here, on the chance of our knowing where to forward it, and
+    that it has been here some time?""",
+
+    """If you’ll be
+    so good as give me your keys, my dear, I’ll attend to all this sort of
+    thing in future."""
+]
+
+CHARLES_DICKENS_SAME_BOOK_MULTIPLE_PARAGRAPH_TESTS = [
     """‘When he
     bought the house, he liked to think that there were rooks about it.’
 
@@ -21,7 +42,34 @@ CHARLES_DICKENS_TESTS = [
     "‘There have not been any since we have lived here,’ said my mother."""
 ]
 
-FYODOR_DOSTOEVSKY_TESTS = [
+CHARLES_DICKENS_TESTS = CHARLES_DICKENS_SAME_BOOK_SINGLE_SENTENCE_TESTS + \
+                        CHARLES_DICKENS_SAME_BOOK_MULTIPLE_PARAGRAPH_TESTS
+
+
+FYODOR_DOSTOEVSKY_SAME_BOOK_SINGLE_SENTENCE_TESTS = [
+    """I tell you again, mother, he is still very ill. Don’t you see it?"
+    Perhaps worrying about us upset him.
+    "We must be patient, and much, much
+    can be forgiven.""",
+
+    """We both worried her so that she gave in; but she
+    wished us to tell you nothing about it until the day.""",
+
+    """The
+    possibility of being loved himself, “a man like me,” as he put it, he
+    ranked among ridiculous suppositions.""",
+
+    """He began talking of the news of the town, of the arrival
+    of the governor's wife, ""with new topics of conversation,"" of an
+    opposition party already formed in the club, of how they were all in a
+    hubbub over the new ideas, and how charmingly this suited him, and so
+    on.""",
+
+    """You treat every one as an equal, and yet every one
+    is afraid of you--that's good."""
+]
+
+FYODOR_DOSTOEVSKY_SAME_BOOK_MULTIPLE_PARAGRAPH_TESTS = [
     """But in spite of this scornful reflection, he was by now looking cheerful
     as though he were suddenly set free from a terrible burden: and he gazed
     round in a friendly way at the people in the room. But even at that
@@ -29,7 +77,36 @@ FYODOR_DOSTOEVSKY_TESTS = [
     not normal."""
 ]
 
-LEO_TOLSTOY_TESTS = [
+FYODOR_DOSTOEVSKY_TESTS = FYODOR_DOSTOEVSKY_SAME_BOOK_SINGLE_SENTENCE_TESTS + \
+                          FYODOR_DOSTOEVSKY_SAME_BOOK_MULTIPLE_PARAGRAPH_TESTS
+
+
+LEO_TOLSTOY_SAME_BOOK_SINGLE_SENTENCE_TESTS = [
+    """If ever at any moment she had been asked what she
+    was thinking of, she could have answered truly: of the same thing, of
+    her happiness and her unhappiness.""",
+
+    """But she saw him not from
+    without, but from within; she saw that here he was not himself; that
+    was the only way she could define his condition to herself.""",
+
+    """Irresistible
+    drowsiness overpowered him, red rings danced before his eyes, and the
+    impression of those voices and faces and a sense of loneliness merged
+    with the physical pain.""",
+
+    """As soon as it rose everyone in the boxes and stalls became silent, and
+    all the men, old and young, in uniform and evening dress, and all the
+    women with gems on their bare flesh, turned their whole attention with
+    eager curiosity to the stage.""",
+
+    """“It’s
+    all right my staying a day with you?” And not waiting for a reply he
+    answered his own question: “You see I was told to find out—well, I am
+    finding out.... Only do let me into the very... into the chief..."""
+]
+
+LEO_TOLSTOY_SAME_BOOK_MULTIPLE_PARAGRAPH_TESTS = [
     """She raised herself on the sofa on which she had been
     lying and replied through the closed door that she did not mean to go
     away and begged to be left in peace."
@@ -49,7 +126,35 @@ LEO_TOLSTOY_TESTS = [
     clear but windy evening."""
 ]
 
-MARK_TWAIN_TESTS = [
+LEO_TOLSTOY_TESTS = LEO_TOLSTOY_SAME_BOOK_SINGLE_SENTENCE_TESTS + \
+                    LEO_TOLSTOY_SAME_BOOK_MULTIPLE_PARAGRAPH_TESTS
+
+
+MARK_TWAIN_SAME_BOOK_SINGLE_SENTENCE_TESTS = [
+    """It was true--as to recent times at least--for there was witness
+    to it, and better witness than a monk; only about twenty or thirty
+    feet of the chain showed wear and use, the rest of it was unworn
+    and rusty.""",
+
+    """It was kind of
+    solemn, drifting down the big, still river, laying on our backs looking
+    up at the stars, and we didn't ever feel like talking loud, and it
+    warn't often that we laughed--only a little kind of a low chuckle.""",
+
+    """But she counted and counted till she got that addled she'd start
+    to count in the basket for a spoon sometimes; and so, three times they
+    come out right, and three times they come out wrong.""",
+
+    """He was eating an apple, and
+    giving a long, melodious whoop, at intervals, followed by a deep-toned
+    ding-dong-dong, ding-dong-dong, for he was personating a steamboat.""",
+
+    """It was
+    not very large; it was iron bound and had been very strong before the
+    slow years had injured it."""
+]
+
+MARK_TWAIN_SAME_BOOK_MULTIPLE_PARAGRAPH_TESTS = [
     """When we saw him last, royalty was just beginning to have a bright side
     for him.  This bright side went on brightening more and more every
     day: in a very little while it was become almost all sunshine and
@@ -57,3 +162,7 @@ MARK_TWAIN_TESTS = [
     his embarrassments departed, and gave place to an easy and confident
     bearing.  He worked the whipping-boy mine to ever-increasing profit."""
 ]
+
+MARK_TWAIN_TESTS = MARK_TWAIN_SAME_BOOK_SINGLE_SENTENCE_TESTS + \
+                   MARK_TWAIN_SAME_BOOK_MULTIPLE_PARAGRAPH_TESTS
+
