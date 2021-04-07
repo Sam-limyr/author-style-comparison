3eaf0107c8536d65b389d4a135f1626ff6f5695f
Sam-limyr <nacuniel@gmail.com>
2021-04-07 21:20:06 +0800

    test (test_cases): Add function to read test cases from all files in a folder.

--- test_cases.py
+++ test_cases.py
@@ -1,4 +1,18 @@
 import os
+import math
+
+
+def get_tests_from_folder(folder, minimum_total_number_of_tests, minimum_test_length):
+    number_of_books = len(os.listdir(folder))
+    number_of_tests_per_book = math.ceil(minimum_total_number_of_tests/number_of_books)
+
+    all_folder_tests = []
+    for filename in os.listdir(folder):
+        all_book_tests = get_tests_from_book(os.path.join(folder, filename), number_of_tests_per_book,
+                                             minimum_test_length)
+        all_folder_tests += all_book_tests
+
+    return all_folder_tests
 
 
 def get_tests_from_book(filename, number_of_tests, minimum_test_length):
@@ -10,18 +24,10 @@ def get_tests_from_book(filename, number_of_tests, minimum_test_length):
     :return: Returns all requested test cases, in a list.
     """
 
-    """
-    Open file object.
-    Count number of lines.
-    Partition based on the number of tests.
-    Open the file object again.
-    At each test case line, keep adding lines until the minimum test length has been reached.
-    """
-
     with open(filename, 'r', encoding='utf-8') as file:
         all_file_lines = list(file.readlines())
         number_of_lines_in_file = len(all_file_lines)
-        partition_size = int(number_of_lines_in_file/number_of_tests)
+        partition_size = math.floor(number_of_lines_in_file/number_of_tests)
 
         all_test_cases = []
         test_case_starting_line_numbers = [partition_size * partition_number
@@ -36,45 +42,27 @@ def get_tests_from_book(filename, number_of_tests, minimum_test_length):
             all_test_cases.append(test_case_text)
 
     for test_case in all_test_cases:
-        assert len(test_case) >= minimum_test_length, "A test case is shorter than expected: {}".format(test_case)
+        assert len(test_case) >= minimum_test_length, "\n\nA test case is shorter than expected. " \
+                                                      "This is most likely because there is insufficient content " \
+                                                      "in one of the test novels: {}".format(test_case)
 
     return all_test_cases
 
+
 # File addresses for test novels
 ROOT = "supplementaryNovels"
-CHARLES_DICKENS_FOLDER = "charles_dickens"
-FYODOR_DOSTOEVSKY_FOLDER = "fyodor_dostoevsky"
-MARK_TWAIN_FOLDER = "mark_twain"
-
-CHARLES_DICKENS_TEST_FILES = [os.path.join(ROOT, CHARLES_DICKENS_FOLDER, filename)
-                              for filename in os.listdir(os.path.join(ROOT, CHARLES_DICKENS_FOLDER))]
-FYODOR_DOSTOEVSKY_TEST_FILES = [os.path.join(ROOT, FYODOR_DOSTOEVSKY_FOLDER, filename)
-                                for filename in os.listdir(os.path.join(ROOT, FYODOR_DOSTOEVSKY_FOLDER))]
-MARK_TWAIN_TEST_FILES = [os.path.join(ROOT, MARK_TWAIN_FOLDER, filename)
-                         for filename in os.listdir(os.path.join(ROOT, MARK_TWAIN_FOLDER))]
+CHARLES_DICKENS_FOLDER = os.path.join(ROOT, "charles_dickens")
+FYODOR_DOSTOEVSKY_FOLDER = os.path.join(ROOT, "fyodor_dostoevsky")
+MARK_TWAIN_FOLDER = os.path.join(ROOT, "mark_twain")
 
 
 # Combined tests
 
-CHARLES_DICKENS_TESTS = get_tests_from_book('supplementaryNovels/charles_dickens/barnabyRudge.txt', 25, 1000)
-# CHARLES_DICKENS_TESTS = CHARLES_DICKENS_DIFFERENT_BOOK_MULTIPLE_PARAGRAPH_TESTS
-                        # CHARLES_DICKENS_SAME_BOOK_SINGLE_SENTENCE_TESTS + \
-                        # CHARLES_DICKENS_SAME_BOOK_MULTIPLE_PARAGRAPH_TESTS
+CHARLES_DICKENS_TESTS = get_tests_from_folder(CHARLES_DICKENS_FOLDER, 150, 1000)
 
+FYODOR_DOSTOEVSKY_TESTS = get_tests_from_folder(FYODOR_DOSTOEVSKY_FOLDER, 150, 1000)
 
-FYODOR_DOSTOEVSKY_TESTS = []
-# FYODOR_DOSTOEVSKY_TESTS = FYODOR_DOSTOEVSKY_DIFFERENT_BOOK_MULTIPLE_PARAGRAPH_TESTS
-                          # FYODOR_DOSTOEVSKY_SAME_BOOK_SINGLE_SENTENCE_TESTS + \
-                          # FYODOR_DOSTOEVSKY_SAME_BOOK_SINGLE_PARAGRAPH_TESTS + \
-                          # FYODOR_DOSTOEVSKY_SAME_BOOK_MULTIPLE_PARAGRAPH_TESTS
-
-# LEO_TOLSTOY_TESTS = LEO_TOLSTOY_SAME_BOOK_SINGLE_SENTENCE_TESTS + \
-#                     LEO_TOLSTOY_SAME_BOOK_MULTIPLE_PARAGRAPH_TESTS
 LEO_TOLSTOY_TESTS = []
 
-MARK_TWAIN_TESTS = []
-# MARK_TWAIN_TESTS = MARK_TWAIN_DIFFERENT_BOOK_MULTIPLE_PARAGRAPH_TESTS
-                   # MARK_TWAIN_SAME_BOOK_SINGLE_SENTENCE_TESTS + \
-                   # MARK_TWAIN_SAME_BOOK_SINGLE_PARAGRAPH_TESTS + \
-                   # MARK_TWAIN_SAME_BOOK_MULTIPLE_PARAGRAPH_TESTS
+MARK_TWAIN_TESTS = get_tests_from_folder(MARK_TWAIN_FOLDER, 150, 1000)
 
