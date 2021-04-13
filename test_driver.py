611ad0815f120bf11030020c24a800478a70c221
Sam-limyr <nacuniel@gmail.com>
2021-04-13 17:11:10 +0800

    feat (test_driver): Add normalization for confidence scores.

--- test_driver.py
+++ test_driver.py
@@ -3,18 +3,24 @@ from feature_engineering import predict_feature_engineering
 from functionwords import predict_k_nearest_neighbours_results
 from collections import Counter
 from test_runner import check_test_results
+from sklearn.preprocessing import scale
 
 
 def main():
 
     print("\n\nGetting doc2vec predictions...\n")
-    doc2vec_results = predict_doc2vec_results()
+    normalized_doc2vec_results = normalize_results(predict_doc2vec_results())
     print("\n\nGetting feature engineering predictions...\n")
-    feature_engineering_results = predict_feature_engineering()
+    normalized_feature_engineering_results = normalize_results(predict_feature_engineering())
     print("\n\nGetting k-Nearest-Neighbours predictions...\n")
-    k_nearest_neighbours_results = predict_k_nearest_neighbours_results()
+    normalized_k_nearest_neighbours_results = normalize_results(predict_k_nearest_neighbours_results())
 
-    combined_results = list(zip(doc2vec_results, feature_engineering_results, k_nearest_neighbours_results))
+    combined_results = list(zip(
+        normalized_doc2vec_results
+        ,        normalized_feature_engineering_results
+        ,        normalized_k_nearest_neighbours_results
+    ))
+
     print("\n\nCombining results into ensemble...\n")
     ensemble_results = [determine_ensemble_answer(result) for result in combined_results]
 
@@ -22,21 +28,38 @@ def main():
 
 
 def determine_ensemble_answer(*args):
-    answers = Counter([element[0] for element in args])
+    results = args[0]
+    answers = Counter([element[0] for element in results])
 
     # If more than 50% of the models have the same answer, return that answer
     most_common_answer, most_common_answer_frequency = answers.most_common(1)[0]
-    if most_common_answer_frequency > 0.5 * len(args):
+    if most_common_answer_frequency > 0.5 * len(results):
+        print("Majority pick: {}".format(most_common_answer))
         return most_common_answer
 
     # Else, if there is a clear plurality winner, return that answer
     _, second_most_common_answer_frequency = answers.most_common(2)[1]
     if most_common_answer_frequency > second_most_common_answer_frequency:
+        print("Plurality pick: {}".format(most_common_answer))
         return most_common_answer
 
     # Else, select the answer with the highest confidence value
-    sorted_answers = sorted(args, key=lambda x: x[1], reverse=True)
+    sorted_answers = sorted(results, key=lambda x: x[1], reverse=True)
+    print("Tiebreak pick: {} | Score {} against {}".format(sorted_answers[0][0], round(float(sorted_answers[0][1]), 4),
+                                                           round(float(sorted_answers[1][1]), 4)))
     return sorted_answers[0][0]
+
+
+def normalize_results(results_list_and_confidence_scores):
+    predictions = [element[0] for element in results_list_and_confidence_scores]
+    confidence_scores = [element[1] for element in results_list_and_confidence_scores]
+    normalized_scores = normalize_confidence_score(confidence_scores)
+    normalized_results = list(zip(predictions, normalized_scores))
+    return normalized_results
+
+
+def normalize_confidence_score(scores):
+    return scale(scores, axis=0, with_mean=True, with_std=True, copy=True)
 
 
 if __name__ == "__main__":
